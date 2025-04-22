64660262de4054e1fd411989ae588122c2d1d7dd
Mehedi Hasan <hasanmehdi89@gmail.com>
2025-04-22 11:10:45 +0600

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Learn math fun way',
+  description: 'Learn Addition, Subsctraction, Multiplication and Division',
   generator: 'v0.dev',
 }
 
